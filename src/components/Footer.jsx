7fd055d01419d70a93c1fa100805360ba84ccc7c
이현빈 <lhb0107@naver.com>
2025-05-14 14:17:50 +0900

    feat : complete Mocking WebSite

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,17 +5,14 @@ const Footer = () => {
   return (
     <footer className="w-full py-4 bg-black-100/55 text-white flex justify-center gap-6">
       <a
-        href="https://www.instagram.com/huzaifahmedz/"
+        href="https://www.instagram.com/hyeonbin418/"
         target="_blank"
         rel="noopener noreferrer"
       >
         <FaInstagram size={24} />
       </a>
-      <a href="https://twitter.comhttps://x.com/HuzaifDev" target="_blank" rel="noopener noreferrer">
-        <FaXTwitter size={24} />
-      </a>
       <a
-        href="https://www.linkedin.com/in/huzaif-ahmed-6abb49273/"
+        href="https://www.linkedin.com/in/%EC%BB%B4%ED%93%A8%ED%84%B0%EA%B3%B5%ED%95%99%EB%B6%80-%EC%9D%B4%ED%98%84%EB%B9%88-3713a6334/?trk=opento_sprofile_goalscard"
         target="_blank"
         rel="noopener noreferrer"
       >
